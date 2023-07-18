780ab92174bef72880bd81fc87868aa4d8f052ee
aung-myint-ko <aungmyintkoanarmala@gmail.com>
2023-07-18 22:49:37 +0630

    adjusting about image size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import Avatar from "../assets/profile3.jpg";
 
 function About() {
   return (
@@ -10,11 +11,11 @@ function About() {
       <h1 className="purple text-center font-black text-2xl sm:text-4xl mb-7 ">
         About Me
       </h1>
-      <div className=" lg:flex lg:flex-row-reverse lg:items-center ">
-        <div className=" w-4/5 md:w-3/6 overflow-hidden mx-auto lg:mx-0 mb-5 lg:mb-0">
+      <div className=" flex flex-col lg:flex lg:flex-row-reverse lg:items-center ">
+        <div className=" w-4/5 md:w-3/6  mx-auto lg:mx-0 mb-5 lg:mb-0">
           <LazyLoadImage
-            className=" w-full h-full max-w-[280px] sm:max-w-xs mx-auto shadow-md rounded-md "
-            src="/images/profile3.jpg"
+            className=" block w-full h-full max-w-[280px] sm:max-w-xs min-h-[340px] sm:min-h-[400px] mx-auto shadow-md rounded-md bg-slate-100 "
+            src={Avatar}
             alt="developer-avater"
           />
         </div>
